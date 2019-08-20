3d0b2063a33382d4c5e703cb467586c5da486909
Diana <d7@hotmail.es>
2019-08-20 15:34:25 +0200

    add a condicional message if there are or not created events

--- src/components/EventsList/EventsList.js
+++ src/components/EventsList/EventsList.js
@@ -3,22 +3,27 @@ import Event from '../Event/Event';
 import PropTypes from 'prop-types';
 
 
-const EventsList = ({events, deleteEvent}) => (
-  <div className='card py-5'>
-    <div className='card-body'>
-      <h2 className='card-title text-center mb-5'>Manage your events here</h2>
-      <div className='events-list'>
-        {events.map(event => (
-          <Event
-            key={event.id}
-            event={event}
-            deleteEvent={deleteEvent}
-          />
-        ))}
+const EventsList = ({events, deleteEvent}) => {
+  const message = Object.keys(events).length === 0 ? 'There are no events'
+                                                   : 'Manage your events here'
+
+  return (
+    <div className='card py-5'>
+      <div className='card-body'>
+        <h2 className='card-title text-center mb-5'>{message}</h2>
+        <div className='events-list'>
+          {events.map(event => (
+            <Event
+              key={event.id}
+              event={event}
+              deleteEvent={deleteEvent}
+            />
+          ))}
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 EventsList.propTypes = {
   events : PropTypes.array.isRequired,
